be57e5f88e5d06d088bc07f05678d776ddfca85c
ruspian <ruspianntb@gmail.com>
2025-01-15 13:10:14 +0800

    01-popupbox

---
+++ 01-popup-box/script.js
@@ -0,0 +1,18 @@
+//  popup box alert
+alert("Hello World!");
+
+//  popup box prompt
+const nama = prompt("Masukkan Nama?");
+alert(`Halo ${nama}`);
+
+//  popup box confirm
+const namaSaya = prompt("Masukkan Nama!");
+confirm(`Apa Benar ${namaSaya} adalah nama anda?`);
+alert(`Halo ${namaSaya}`);
+
+// let lagi = true;
+// while (lagi) {
+//     const nama = prompt("Masukkan Nama!");
+//     alert(`Halo ${nama}`);
+//     lagi = confirm("Lagi?");
+// }
