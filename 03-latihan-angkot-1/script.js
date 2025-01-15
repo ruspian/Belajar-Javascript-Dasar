4ad332d1e69a06e09fc204d7f0c1817131194918
ruspian <ruspianntb@gmail.com>
2025-01-15 13:46:35 +0800

    perbaiki latihan angkot

--- 03-latihan-angkot-1/script.js
+++ 03-latihan-angkot-1/script.js
@@ -1,6 +1,11 @@
 // latihan angkot
 let jumlahAngkot = 10;
-let angkotBeroperasi = 6;
+let angkotBeroperasi = 1;
 for (let noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++) {
   console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
 }
+
+// while (angkotBeroperasi <= jumlahAngkot) {
+//   console.log("Angkot No. " + angkotBeroperasi + " sedang tidak beroperasi.");
+//   angkotBeroperasi++;
+// }
