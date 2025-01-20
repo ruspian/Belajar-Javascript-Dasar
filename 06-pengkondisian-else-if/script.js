aa94cb1cd4735ddfed69fa0b51cdc2b5232f91a5
ruspian <ruspianntb@gmail.com>
2025-01-21 03:07:25 +0800

    06 pengkondisian else if

---
+++ 06-pengkondisian-else-if/script.js
@@ -0,0 +1,9 @@
+let angka = prompt("Masukkan angka: ");
+
+if (angka % 2 == 0) {
+  alert(angka + " adalah bilangan genap");
+} else if (angka % 2 == 1) {
+  alert(angka + " adalah bilangan ganjil");
+} else {
+  alert("Yang anda masukkan bukan angka");
+}
