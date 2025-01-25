26b7f1e7be45968f224b6c9b2fa8257ba71b548e
ruspian <ruspianntb@gmail.com>
2025-01-25 09:07:51 +0800

    parameter dan argumen dan mengetahui sudo variable arguments

--- 13-function/script.js
+++ 13-function/script.js
@@ -23,3 +23,30 @@ function balok(panjang, lebar, tinggi) {
 }
 
 console.log("volume balok = " + balok(4, 5, 6));
+
+// parameter dan argument
+function tambah(a, b) {
+  // a dan b adalah parameter
+  return a + b;
+}
+
+tambah(1, 2); // 1 dan 2 adalah argument
+
+// fungsi didalam fungsi
+function kali(a, b) {
+  return a * b;
+}
+
+const hasil = kali(tambah(1, 2), kali(3, 4));
+console.log(hasil);
+
+// sudo variable arguments
+function tambahLagi() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i]; // total = total + arguments[i]
+  }
+  return total;
+}
+
+console.log(tambahLagi(1, 2, 3, 4, 5));
