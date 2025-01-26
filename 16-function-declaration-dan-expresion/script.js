dc9c4a148d15a6cd987a0f83c84c256fb6b05d0d
ruspian <ruspianntb@gmail.com>
2025-01-26 21:57:25 +0800

    16-function-declaration-dan-expresion

---
+++ 16-function-declaration-dan-expresion/script.js
@@ -0,0 +1,15 @@
+// fungsi declaration dan expresion
+
+// penulisan function declaration
+function tambah(a, b) {
+  return a + b;
+}
+
+console.log(tambah(1, 2));
+
+// penulisan function expression
+const kali = function (a, b) {
+  return a * b;
+};
+
+console.log(kali(3, 4));
