0a946448f87940068382815aa0b879dae50952a8
ruspian <ruspianntb@gmail.com>
2025-01-26 22:16:40 +0800

    17 array

---
+++ 17-array/script.js
@@ -0,0 +1,19 @@
+// array => sebuah variable tapi lebih sakti
+
+// penulisan array
+const nama = ["ucup", "otong", "dudung"];
+const angka = [1, 2, 3, 4, 5];
+const hewan = ["kucing", "kelinci", "kambing", "kuda", "ayam"];
+const acak = [1, "ucup", true, "otong"];
+
+console.log(nama);
+console.log(angka);
+console.log(hewan);
+console.log(acak);
+
+// mengakses array
+console.log(hewan[0]);
+console.log(hewan[1]);
+console.log(hewan[2]);
+console.log(hewan[3]);
+console.log(hewan[4]);
