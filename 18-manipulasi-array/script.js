a63cdc89c3b3e7fa42f7644ccc9ffd0357659932
ruspian <ruspianntb@gmail.com>
2025-01-27 08:41:45 +0800

    18-manipulasi-array

---
+++ 18-manipulasi-array/script.js
@@ -0,0 +1,45 @@
+// manipulasi array
+
+// 1. menambah isi array
+
+// cara manual
+// let nama = [];
+// nama[0] = "ucup";
+// nama[1] = "otong";
+// nama[2] = "dudung";
+
+// console.log(nama);
+
+// 2.menghapus isi array
+// cara manual
+// let nama = ["ucup", "otong", "dudung"];
+// nama[0] = undefined;
+
+// console.log(nama);
+
+// 3. menampilkan isi array dengan loop
+// let nama = ["ucup", "otong", "dudung"];
+// for (let i = 0; i < nama.length; i++) {
+//   console.log("Mahasiswa ke-" + (i + 1) + " : " + nama[i]);
+// }
+
+// method pada array => method adalah function yang ada di dalam array
+// 1. join => menggabungkan elemen array menjadi string
+let nama = ["ucup", "otong", "dudung"];
+// console.log(nama.join(" - "));
+
+// 2. push => menambah elemen di akhir array
+// nama.push("dodi");
+// console.log(nama);
+
+// 3. pop => menghapus elemen di akhir array
+// nama.pop();
+// console.log(nama);
+
+// 4. unshift => menambah elemen di awal array
+// nama.unshift("surucup");
+// console.log(nama);
+
+// 5. shift => menghapus elemen di awal array
+nama.shift();
+console.log(nama);
