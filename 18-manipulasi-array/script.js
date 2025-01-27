b727a018a5e0d4b250a323959932c8a7b865ca4e
ruspian <ruspianntb@gmail.com>
2025-01-27 09:09:47 +0800

    method foreach, map,sort pada arrayy

--- 18-manipulasi-array/script.js
+++ 18-manipulasi-array/script.js
@@ -25,7 +25,7 @@
 
 // method pada array => method adalah function yang ada di dalam array
 // 1. join => menggabungkan elemen array menjadi string
-let nama = ["ucup", "otong", "dudung"];
+// let nama = ["ucup", "otong", "dudung"];
 // console.log(nama.join(" - "));
 
 // 2. push => menambah elemen di akhir array
@@ -53,3 +53,37 @@ let nama = ["ucup", "otong", "dudung"];
 // rumus => array.slice(indexAwal, indexAkhir)
 // let namaSaya = nama.slice(1, 3);
 // console.log(namaSaya);
+
+// 8. forEach => menjalankan function untuk setiap elemen array / perulangan dan mengembalikan nilai string
+// let angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let nama = ["ucup", "otong", "dudung"];
+// angka.forEach(function (angka) {
+//   console.log(angka);
+// });
+
+// index pada forEach
+// nama.forEach(function (nama, index) {
+//   console.log("Mahasiswa ke-" + (index + 1) + " : " + nama);
+// });
+
+// 9. map => menjalankan function untuk setiap elemen array / perulangan dan mengembalikan nilai array
+// let angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let angkaKu = angka.map(function (angka) {
+//   return angka * 2;
+// });
+// console.log(angkaKu);
+
+// 10. sort => mengurutkan elemen array
+// jika ada puluhan
+// const angka = [1, 2, 4, 9, 7, 10, 20, 6, 8, 5, 3];
+// console.log(angka.join(" - "));
+// angka.sort(function (a, b) {
+//   return a - b;
+// });
+// console.log(angka.join(" - "));
+
+// jika tidak ada puluhan
+const angka = [1, 2, 4, 9, 7, 6, 8, 5, 3];
+console.log(angka.join(" - "));
+angka.sort();
+console.log(angka.join(" - "));
