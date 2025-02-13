b6461dcbc34a15f16c37b8220576e1cf9cca3e3d
ruspian <ruspianntb@gmail.com>
2025-02-13 20:58:40 +0800

    30-template-riteral

---
+++ 30-template-riteral/script.js
@@ -0,0 +1,12 @@
+// template literal
+// template literal adalah cara untuk membuat string
+// template literal digunakan untuk membuat string dengan menggunakan backtick ``
+
+const nama = "ucup";
+const umur = 20;
+
+console.log(`halo nama saya ${nama}, saya ${umur} tahun`);
+
+// menjalankan ternari operator dalam template literal
+const x = 10;
+console.log(`x adalah bilangan: ${x % 2 == 0 ? "genap" : "ganjil"}`);
